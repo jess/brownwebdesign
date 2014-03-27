ca5f6c5b1b2accc957c503f57131619150228358
Jess Brown <jess@brownwebdesign.com>
2014-03-27 15:32:48 -0400

    redirect to ssl if on payments page

---
+++ app/controllers/concerns/handle_ssl.rb
@@ -0,0 +1,31 @@
+module HandleSsl
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :redirect_to_ssl, :redirect_to_normal_domain if Rails.env.production?
+  end
+
+  def redirect_to_ssl
+    if on_ssl_required_page? && on_normal_domain?
+      redirect_to "https://brownwebdesign.herokuapp.com#{request.path}" 
+    end
+  end
+
+  def redirect_to_normal_domain
+    if !on_normal_domain? && !on_ssl_required_page?
+      redirect_to "http://www.brownwebdesign.com#{request.path}"
+    end
+  end
+
+  def on_normal_domain?
+    request.domain == "brownwebdesign.com"
+  end
+
+  def on_ssl_required_page?
+    ssl_only_list.include? request.path
+  end
+
+  def ssl_only_list
+    ["/payments"]
+  end
+end
