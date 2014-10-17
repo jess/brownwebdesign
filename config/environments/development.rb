6e23522bafa35de1e44e8015f67e7968382383ab
Jess Brown <jess@brownwebdesign.com>
2014-10-17 15:59:00 -0400

    add raffle url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Brownwebdesign::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  ::RAFFLE_URL = "http://localhost:3001"
 end
